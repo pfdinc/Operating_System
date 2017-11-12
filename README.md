bb4707df876a0095ce2e1d6b296d2037b82f1708
Pushkin Feleke <pfdinc@gmail.com>
2017-11-11 23:08:45 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Operating_System
+**Create a program to calculate the letter occurrences in a plain text file. **
+
+Overview 
+
+Calculating occurrences of each character in a text file can be useful to build statistics which can serve to break certain encoding (cryptanalysis).  Frequency analysis is based on the fact that, in any given stretch of written language, certain letters and combinations of letters occur with varying frequencies. For instance, given a section of English language, E, T, A and O are the most common, while Z, Q and X are rare. Likewise, TH, ER, ON, and AN are the most common pairs of letters (called bigrams), and SS, EE, TT, and FF are the most common repeats. 
+
+Instructions 
+
+Write a program which reads its input from a plain text file (containing letters, punctuation marks, new lines and digits) given as input (read the filename from the standard input), and calculates a statistic based on the frequency of each letter. For each letter i, there should be a separate process Pi who is responsible to return the frequency for that particular letter. 
+
+Notes
+
+```
+ 	 The text file will be given as input, no 	hardcoding allowed  
+ 
+ 	 Each letter frequency should be returned by a 	separate process.
+ 
+  	 In the text capital and lower case letter should 	be considered the same (ex. A = a). 
+  
+ 	 Punctuation marks such as [, ! ? - .] and digits 		are to be considered, but not to be mentioned in 		the stats.
+  
+     For each letter (letter pair ex. A-a) a separate 	process should be lunched considering fork()! 
+   ```
---
+++ main.cpp
@@ -0,0 +1,110 @@
+#include <iostream> 
+#include <stdio.h>
+#include <fstream>
+#include <vector>
+#include <cstring>
+#include <string> 
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+using namespace std;
+
+
+int main(int argc, char **arg){
+    
+if ((arg[1] != NULL) && (arg[1] == '\0')) {
+   printf("c is empty\n");
+}
+   int size = 4;
+	char str[80];
+	char plus[250];
+	char list[] = {'E' , 'T' , 'A' , 'O'};
+	int registered [size];
+
+     for(int n = 0 ; n< size; n++){
+
+		registered [n]= 0;
+	}
+	//char text[250];
+	//size = sizeof(list)/sizeof(list[0]);
+	//cout<< size << endl;
+	string text;
+	int freq[128]; 
+	char createFile[205];
+	char userInput[250];
+	/*cout << "File name you will like to create (w/o "".txt"")->>$  ";
+	cin >> arg;
+	*/
+	strcpy(str, arg[1]);
+	strcat(str, ".txt");
+	//ofstream myFile;
+	//myFile.open(arg[1]);
+	cout << endl;
+	cout << "opening " << arg[1] <<  " File " << endl;
+
+	//std::getline(std::cin, text);
+	//myFile<<text;
+   	//myFile.close();
+   	ifstream inFile ;
+	char mychar;
+   	inFile.open(arg[1],ios::in);
+	
+   if (!inFile)
+   {
+      cout << "The input file could not be opened." <<endl;
+      return 1;
+   }
+
+
+   for (int k = 0; k < 128; k++)
+   {
+      freq[k] = 0;
+   }
+     pid_t pid,wpid ;
+     //int status;
+      pid = fork();
+
+     for (int i = 0; i < size; i++)
+     {
+         
+
+         if ( pid  == 0)
+         {
+         	mychar = inFile.get();
+         	//cout <<"~~" << i <<"~~"<< endl;
+
+	          while (mychar != EOF)
+			   {
+			      //cout << mychar;
+			      mychar = toupper(mychar);
+			      freq[mychar]++;
+			      mychar = inFile.get();
+			   }
+
+			   cout << endl << "A to Z " << endl;
+			  for (char ch = 'A'; ch <= 'Z'; ch++)
+			  {
+			      cout << ch << " : " << freq[ch] << endl;
+			  }
+			
+			  //@TODO: save funcion / switch statement!
+		
+		_exit(0);
+		 
+           
+        }
+        else 
+        {
+
+        	 int status;
+        	 //system("command arg1 argc");
+			waitpid(pid, &status , 0);
+			 
+	  }
+
+     }
+ 
+  return 0;}
