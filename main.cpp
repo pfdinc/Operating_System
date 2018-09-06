dd59ae32e676b1d3aaec32e7a3cf169b5116a003
Pushkin Feleke <pfdinc@gmail.com>
2018-09-06 09:35:46 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <iostream> 
+#include <iostream>
 #include <stdio.h>
 #include <fstream>
 #include <vector>
 #include <cstring>
-#include <string> 
+#include <string>
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
@@ -13,98 +13,84 @@
 using namespace std;
 
 
-int main(int argc, char **arg){
-    
-if ((arg[1] != NULL) && (arg[1] == '\0')) {
-   printf("c is empty\n");
-}
-   int size = 4;
-	char str[80];
-	char plus[250];
-	char list[] = {'E' , 'T' , 'A' , 'O'};
-	int registered [size];
+int main(int argc, char **arg) {
 
-     for(int n = 0 ; n< size; n++){
+    if ((arg[1] != NULL) && (arg[1] == '\0')) {
+        printf("c is empty\n");
+    }
+    int size = 4;
+    char str[80];
+    char plus[250];
+    char list[] = {'E' , 'T' , 'A' , 'O'};
+    int registered [size];
 
-		registered [n]= 0;
-	}
-	//char text[250];
-	//size = sizeof(list)/sizeof(list[0]);
-	//cout<< size << endl;
-	string text;
-	int freq[128]; 
-	char createFile[205];
-	char userInput[250];
-	/*cout << "File name you will like to create (w/o "".txt"")->>$  ";
-	cin >> arg;
-	*/
-	strcpy(str, arg[1]);
-	strcat(str, ".txt");
-	//ofstream myFile;
-	//myFile.open(arg[1]);
-	cout << endl;
-	cout << "opening " << arg[1] <<  " File " << endl;
+    for(int n = 0 ; n< size; n++) {
 
-	//std::getline(std::cin, text);
-	//myFile<<text;
-   	//myFile.close();
-   	ifstream inFile ;
-	char mychar;
-   	inFile.open(arg[1],ios::in);
-	
-   if (!inFile)
-   {
-      cout << "The input file could not be opened." <<endl;
-      return 1;
-   }
+        registered [n]= 0;
+    }
+
+    string text;
+    int freq[128];
+    char createFile[205];
+    char userInput[250];
+
+    strcpy(str, arg[1]);
+    strcat(str, ".txt");
+
+    cout << endl;
+    cout << "opening " << arg[1] <<  " File " << endl;
+
+    ifstream inFile ;
+    char mychar;
+    inFile.open(arg[1],ios::in);
+
+    if (!inFile)
+    {
+        cout << "The input file could not be opened." <<endl;
+        return 1;
+    }
 
 
-   for (int k = 0; k < 128; k++)
-   {
-      freq[k] = 0;
-   }
-     pid_t pid,wpid ;
-     //int status;
-      pid = fork();
+    for (int k = 0; k < 128; k++)
+    {
+        freq[k] = 0;
+    }
+    pid_t pid,wpid ;
+    pid = fork();
 
-     for (int i = 0; i < size; i++)
-     {
-         
+    for (int i = 0; i < size; i++)
+    {
 
-         if ( pid  == 0)
-         {
-         	mychar = inFile.get();
-         	//cout <<"~~" << i <<"~~"<< endl;
 
-	          while (mychar != EOF)
-			   {
-			      //cout << mychar;
-			      mychar = toupper(mychar);
-			      freq[mychar]++;
-			      mychar = inFile.get();
-			   }
+        if ( pid  == 0)
+        {
+            mychar = inFile.get();
 
-			   cout << endl << "A to Z " << endl;
-			  for (char ch = 'A'; ch <= 'Z'; ch++)
-			  {
-			      cout << ch << " : " << freq[ch] << endl;
-			  }
-			
-			  //@TODO: save funcion / switch statement!
-		
-		_exit(0);
-		 
-           
+            while (mychar != EOF)
+            {
+                mychar = toupper(mychar);
+                freq[mychar]++;
+                mychar = inFile.get();
+            }
+
+            cout << endl << "A to Z " << endl;
+            for (char ch = 'A'; ch <= 'Z'; ch++)
+            {
+                cout << ch << " : " << freq[ch] << endl;
+            }
+
+            _exit(0);
+
         }
-        else 
+        else
         {
 
-        	 int status;
-        	 //system("command arg1 argc");
-			waitpid(pid, &status , 0);
-			 
-	  }
+            int status;
+            waitpid(pid, &status , 0);
 
-     }
- 
-  return 0;}
+        }
+
+    }
+
+    return 0;
+}
